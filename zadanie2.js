f2849832183fd5a51825708652eebe104e76ead5
Radoslaw Fabisiak <radoslawfabisiak@gmail.com>
2017-10-11 12:23:19 +0200

    Initial commit. Zadanie 2 z tygodnia pierwszego skonczone.

---
+++ zadanie2.js
@@ -0,0 +1,13 @@
+(function($){
+	
+	$(document).ready(function(){
+		
+		var a = $("div.grid, div.grid-12");
+		var b = $(".nav a[href^='http']");
+		var c = $("input:checkbox:checked, input:radio:checked");
+		var d = $("div#text p:first:empty");
+		var e = $(".pagination-item").not("span");
+		
+	});
+	
+})(jQuery);
